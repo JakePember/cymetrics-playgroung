6aa8d095e93d665d7bb807c801a0e26f14d9e44d
jacobbles <general@bles.co>
2021-09-20 22:43:56 -0500

    - fixes async await issue

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,12 +3,11 @@ const series = require('async').series
 const {exec} = require('child_process');
 
 module.exports = (on, config) => {
-    on('before:run', async (config) => {
-        // "test" : "npm run clean-reports && npx cypress run && npm run posttest",
-        await series([() => exec('npm run clean-reports')]);
+    on('before:run', (config) => {
+        series([() => exec('npm run clean-reports')]);
     })
     on('after:run', async (config) => {
-        await series([
+        series([
             () => exec('npm run posttest')
         ]);
         await cymetrics.balance(config)
